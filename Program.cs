92abb287eb6644cb09572767ea82bbf896df869c
Benjamin Johnson <benjamin.johnson@Benjamins-MacBook-Air.local>
2025-03-20 09:11:22 -0600

    Finished The Intro

--- Program.cs
+++ Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Benjamin Johnson, Lab 6 Typing Trainer, March 20th
+Console.Clear();
+
+Console.WriteLine(@"
+                                      Welcome to the Typing Trainer!
+You'll be given a short story to type out. If you make a mistake, you will not be able to go back and fix it.
+                 Once you're done, your Words Per Minute and Accuracy will be given to you.");
